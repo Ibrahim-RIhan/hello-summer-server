d91f75e824a1138dd2cfd8961ba8b1c6b93f6a29
rihan010 <ibrhimrihan000@gmail.comm>
2023-06-09 11:38:11 +0600

    instructor api  creted

--- index.js
+++ index.js
@@ -43,12 +43,25 @@ app.post('/users', async (req, res) => {
   const result = await usersCollection.insertOne(user);
   res.send(result)
 })
+
 app.patch('users/admin/:id', async (req, res) => {
   const id = req.params.id;
   const query = { _id : new ObjectId(id)};
   const updatedDoc = {
     $set : {
       role : 'admin'
+    }
+  }
+  const result = await usersCollection.updateOne(query, updatedDoc);
+  res.send(result);
+})
+
+app.patch('users/instructor/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id : new ObjectId(id)};
+  const updatedDoc = {
+    $set : {
+      role : 'instructor'
     }
   }
   const result = await usersCollection.updateOne(query, updatedDoc);
