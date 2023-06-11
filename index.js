b2a822183c1ec34262b320e8ff5790013a2fbb4b
rihan010 <ibrhimrihan000@gmail.comm>
2023-06-11 22:00:27 +0600

    selected class get api added

--- index.js
+++ index.js
@@ -162,6 +162,12 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/selectedClass/:email' , async (req, res) => {
+      const email = req.params.email
+      const filter = {email : email}
+      const result = await selectedClassCollection.find(filter).toArray()
+      res.send(result)
+    })
 
 
 
