3dd5faed777315076fa6692c5b3f05929704de18
rihan010 <ibrhimrihan000@gmail.comm>
2023-06-12 11:42:48 +0600

    payment intent api addded

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.PAYMENT_GATEWAY - SK)
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aibqdpa.mongodb.net/?retryWrites=true&w=majority`;
@@ -175,6 +176,19 @@ async function run() {
       const result = await selectedClassCollection.deleteOne(query);
       res.send(result);
     })
+
+    app.post('/create-payment-intent', async (req, res)=>{
+      const { price } = req.body;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
 
 
     // Send a ping to confirm a successful connection
